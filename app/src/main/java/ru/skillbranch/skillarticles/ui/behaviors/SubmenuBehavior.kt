66d67c17c30eee87d95409d6c9ea67428926cc38
Kirill <kirill.189452@gmail.com>
2021-06-17 13:14:41 +0300

    final

---
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/behaviors/SubmenuBehavior.kt
@@ -0,0 +1,48 @@
+package ru.skillbranch.skillarticles.ui.behaviors
+
+import android.util.Log
+import android.view.View
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.ViewCompat
+import androidx.core.view.marginRight
+import ru.skillbranch.skillarticles.ui.custom.ArticleSubmenu
+import ru.skillbranch.skillarticles.ui.custom.Bottombar
+
+class SubmenuBehavior : CoordinatorLayout.Behavior<ArticleSubmenu>(){
+
+    override fun layoutDependsOn(
+        parent: CoordinatorLayout,
+        child: ArticleSubmenu,
+        dependency: View
+    ): Boolean {
+        return dependency is Bottombar
+    }
+
+    override fun onDependentViewChanged(
+        parent: CoordinatorLayout,
+        child: ArticleSubmenu,
+        dependency: View
+    ): Boolean {
+        return if(child.isOpen && dependency is Bottombar && dependency.translationY >= 0){
+            animate(child, dependency)
+            true
+        }else false
+    }
+
+    private fun animate(child: View, dependency: Bottombar) {
+        val fraction = dependency.translationY/dependency.minHeight
+        child.translationX = (child.width + child.marginRight) * fraction
+    }
+
+    override fun onStartNestedScroll(
+        coordinatorLayout: CoordinatorLayout,
+        child: ArticleSubmenu,
+        directTargetChild: View,
+        target: View,
+        axes: Int,
+        type: Int
+    ): Boolean {
+        return axes == ViewCompat.SCROLL_AXIS_VERTICAL
+    }
+
+}
--- app/src/main/java/ru/skillbranch/skillarticles/ui/custom/ArticleSubmenu.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/custom/ArticleSubmenu.kt
@@ -7,18 +7,20 @@ import android.util.AttributeSet
 import android.view.View
 import android.view.ViewAnimationUtils
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.animation.doOnEnd
 import androidx.core.animation.doOnStart
 import com.google.android.material.shape.MaterialShapeDrawable
 import ru.skillbranch.skillarticles.R
 import ru.skillbranch.skillarticles.extensions.dpToPx
+import ru.skillbranch.skillarticles.ui.behaviors.SubmenuBehavior
 import kotlin.math.hypot
 
 class ArticleSubmenu @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-): ConstraintLayout(context, attrs, defStyleAttr) {
+): ConstraintLayout(context, attrs, defStyleAttr), CoordinatorLayout.AttachedBehavior {
 
     var isOpen = false
     private var centerX = context.dpToPx(200)
@@ -36,6 +38,8 @@ class ArticleSubmenu @JvmOverloads constructor(
         isOpen = true
         animatedShow()
     }
+
+
 
     fun close(){
         if(!isOpen || !isAttachedToWindow) return
@@ -111,6 +115,7 @@ class ArticleSubmenu @JvmOverloads constructor(
         }
     }
 
+    override fun getBehavior(): CoordinatorLayout.Behavior<*> = SubmenuBehavior()
 
 
 }
